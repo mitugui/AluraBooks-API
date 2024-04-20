7703298bff2fcd502371fd4c30238546a5996313
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-04-19 21:29:05 -0300

    Adicionado Cors e permitindo acesso de qualquer origem

--- alura-books/app.js
+++ alura-books/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const bookRoute = require("./routes/book")
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/books', bookRoute)
 
